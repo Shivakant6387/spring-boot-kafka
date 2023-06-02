2be9146fa11a6babcdb6d3fe994c26a51a9b7aed
Shivakant6387 <shivakant5875@gmail.com>
2023-06-02 12:58:47 +0530

    consumer and producer

---
+++ src/main/java/com/example/springbootkafka/AppConstants.java
@@ -0,0 +1,6 @@
+package com.example.springbootkafka;
+
+public class AppConstants {
+    public static final String TOPIC_NAME = "Shiva-topic";
+    public static final String GROUP_ID = "group_id";
+}
